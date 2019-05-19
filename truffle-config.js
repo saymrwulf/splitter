1d64613d7bc685f09ef2d2ee6f379c3b46ff336f
oho <oho@MBP3.fritz.box>
2019-05-19 13:50:03 +0200

    after truffle init & empty contract splitter

---
+++ truffle-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  /*
+  networks: {
+    development: {    // this one is optional and reduces "failing fast" dummerweise
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    test: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    net42:{
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "42"
+    },
+    ropsten: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "3"
+    }
+  }
+*/
+};
