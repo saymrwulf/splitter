f24f1d08266c941764c20b94aab5de6efab3f2ee
oho <oho@MBP3.fritz.box>
2019-05-19 14:43:10 +0200

    splitter contract 1st draft; migrations changed; truffle_config.js changed

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,24 +1,21 @@
 pragma solidity >=0.4.25 <0.6.0;
 
-contract MetaCoin {
-	mapping (address => uint) balances;
+contract Splitter {
+	mapping (address => uint) public balances;
 
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+	event Transfer(address indexed _from, address indexed _to1, address indexed _to2, uint256 _value);
 
 	constructor() public {
-		balances[tx.origin] = 10000;
+		balances[msg.sender] = 10000;
 	}
 
-	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
+	function sendSplitCoin(address receiver1, address receiver2, uint amount) public returns(bool sufficient) {
 		if (balances[msg.sender] < amount) return false;
 		balances[msg.sender] -= amount;
-		balances[receiver] += amount;
-		emit Transfer(msg.sender, receiver, amount);
+		balances[receiver1] += amount/2;
+		balances[receiver2] += amount/2;
+		emit Transfer(msg.sender, receiver1, receiver2, amount);
 		return true;
-	}
-
-	function getBalanceInEth(address addr) public view returns(uint){
-		return ConvertLib.convert(getBalance(addr),2);
 	}
 
 	function getBalance(address addr) public view returns(uint) {
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,8 @@
 const Migrations = artifacts.require("Migrations");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
+  console.log("network: ", network);
+  console.log("accounts: ", accounts);
+  
   deployer.deploy(Migrations);
 };
